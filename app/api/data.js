615e6d135132a3ce17ff345b63f1bce3255e5c8d
jomoo02 <jomootgrn88@naver.com>
2024-04-13 22:09:35 +0900

    feat: 포켓몬 검색시 영문 이름도 검색 가능하도록 변경

--- app/api/data.js
+++ app/api/data.js
@@ -1,7 +1,6 @@
 'use server';
 
 import PokeModel from '../models/Poke.mjs';
-import { checkTextNumberType } from '../lib/utils';
 
 export async function fetchPokes(index) {
   try {
@@ -26,61 +25,6 @@ export async function fetchPokes(index) {
       .lean();
 
     return result;
-  } catch (error) {
-    console.error(error);
-    return [];
-  }
-}
-
-const filterById = (pokes) => {
-  const idSet = new Set();
-  const duplicationIdSet = new Set();
-
-  const filteringPoeks = pokes.filter(({ id }) => {
-    if (!idSet.has(id)) {
-      idSet.add(id);
-      return true;
-    }
-    duplicationIdSet.add(id);
-    return false;
-  });
-
-  return filteringPoeks.map((poke) => {
-    if ([...duplicationIdSet].find((id) => id === poke.id)) {
-      return {
-        ...poke,
-        form: {
-          en: 'default',
-          ko: 'default',
-        },
-      };
-    }
-    return poke;
-  });
-};
-
-export async function fetchSearchPokes(text) {
-  try {
-    const isTextNumber = checkTextNumberType(text);
-    const query = isTextNumber ? { no: Number(text) } : { 'name.ko': { $regex: text, $options: 'i' } };
-
-    const projection = {
-      name: 1,
-      id: 1,
-      types: 1,
-      sprity: 1,
-      no: 1,
-      form: 1,
-      key: 1,
-      _id: 0,
-    };
-
-    const result = await PokeModel
-      .find(query, projection)
-      .sort({ no: 1, id: 1 })
-      .lean();
-
-    return filterById(result);
   } catch (error) {
     console.error(error);
     return [];
---
+++ app/api/search.js
@@ -0,0 +1,72 @@
+'use server';
+
+import { checkTextLanguageKo, checkTextNumberType } from '../lib/utils';
+import PokeModel from '../models/Poke.mjs';
+
+function filterById(pokes) {
+  const idSet = new Set();
+  const duplicationIdSet = new Set();
+
+  const filteringPoeks = pokes.filter(({ id }) => {
+    if (!idSet.has(id)) {
+      idSet.add(id);
+      return true;
+    }
+    duplicationIdSet.add(id);
+    return false;
+  });
+
+  return filteringPoeks.map((poke) => {
+    if ([...duplicationIdSet].find((id) => id === poke.id)) {
+      return {
+        ...poke,
+        form: {
+          en: 'default',
+          ko: 'default',
+        },
+      };
+    }
+    return poke;
+  });
+}
+
+function decideSearchQuery(text) {
+  const isTextNumber = checkTextNumberType(text);
+  if (isTextNumber) {
+    return ({ no: Number(text) });
+  }
+
+  const isTextLanguageKo = checkTextLanguageKo(text);
+  if (isTextLanguageKo) {
+    return ({ 'name.ko': { $regex: text, $options: 'i' } });
+  }
+
+  return ({ 'name.en': { $regex: text, $options: 'i' } });
+}
+
+export async function fetchSearchPokes(text) {
+  try {
+    const query = decideSearchQuery(text);
+
+    const projection = {
+      name: 1,
+      id: 1,
+      types: 1,
+      sprity: 1,
+      no: 1,
+      form: 1,
+      key: 1,
+      _id: 0,
+    };
+
+    const result = await PokeModel
+      .find(query, projection)
+      .sort({ no: 1, id: 1 })
+      .lean();
+
+    return filterById(result);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
