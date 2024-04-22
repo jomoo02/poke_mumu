846f070c442e5dfcbfba99c32920528124e40167
jomoo02 <jomootgrn88@naver.com>
2024-04-22 20:47:07 +0900

    test: image unoptimized

--- app/ui/pokeCard/card.jsx
+++ app/ui/pokeCard/card.jsx
@@ -37,7 +37,7 @@ export default function Card({
                 style={{
                   objectFit: 'contain',
                 }}
-                priority
+                loading="lazy"
               />
             </div>
           </Link>
