f4ae40efb1f41cd51d13daef0cd1a72cf0cdfb6f
jomoo02 <jomootgrn88@naver.com>
2024-05-20 01:54:45 +0900

    refactor: detail page pre-evolution image error시 fallback 이미지 경로 설정

--- app/ui/detail/moves/method/pre-evolution.jsx
+++ app/ui/detail/moves/method/pre-evolution.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import MethodMoves from './method-moves';
 
 function Content({ moveData: { preIds } }) {
   const sprityUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons';
+  const alterSprityUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+
+  const initialImageUrls = preIds.map((id) => ({
+    id,
+    src: `${sprityUrl}/${id}.png`,
+    fallback: `${alterSprityUrl}/${id}.png`,
+  }));
+
+  const [imageUrls, setImageUrls] = useState(initialImageUrls);
+
+  const handleImageError = (index) => {
+    setImageUrls((preImageUrls) => {
+      const newImageUrls = [...preImageUrls];
+      newImageUrls[index] = {
+        ...newImageUrls[index],
+        src: newImageUrls[index].fallback,
+      };
+      return newImageUrls;
+    });
+  };
+
   return (
     <div className="col-span-1 flex flex-col md:flex-row">
-      {preIds.map((id) => (
+      {imageUrls.map(({ id, src }, index) => (
         <Image
           key={id}
-          src={`${sprityUrl}/${id}.png`}
+          src={src}
           alt={id}
           width={35}
           height={25}
           style={{ width: 35, height: 25 }}
+          onError={() => handleImageError(index)}
         />
       ))}
     </div>
