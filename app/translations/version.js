291d874304958ff2e86f47d9643ab432d2c5148d
jomoo02 <jomootgrn88@naver.com>
2024-06-16 19:48:37 +0900

    bug: fix Module not found: Can't resolve '@tabler/icons-react'

---
+++ app/translations/version.js
@@ -0,0 +1,51 @@
+const versionKo = {
+  red: '레드',
+  blue: '블루',
+  yellow: '옐로우',
+
+  gold: '골드',
+  silver: '실버',
+  crystal: '크리스탈',
+
+  ruby: '루비',
+  sapphire: '사파이어',
+  firered: '파이어레드',
+  leafgreen: '리프그린',
+  emerald: '에메랄드',
+
+  diamond: '다이야몬드',
+  pearl: '펄',
+  platinum: '플래티넘',
+  heartgold: '하트골드',
+  soulsilver: '소울실버',
+
+  black: '블랙',
+  white: '화이트',
+  'black-2': '블랙2',
+  'white-2': '화이트2',
+
+  x: 'x',
+  y: 'y',
+
+  'omega-ruby': '오메가 루비',
+  'alpha-sapphire': '알파 사파이어',
+
+  sun: '썬',
+  moon: '문',
+  'ultra-sun': '울트라 썬',
+  'ultra-moon': '울트라 문',
+
+  sword: '소드',
+  shiled: '실드',
+  'brilliant-diamond': '브다',
+  'shining-pearl': '샤펄',
+  'lets-go-pikachu': '렛츠고피',
+  'lets-go-eevee': '렛츠고이',
+
+  scarlet: '스칼렛',
+  violet: '바이올렛',
+};
+
+export {
+  versionKo,
+};
