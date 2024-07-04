2a453083e89b72329741c35c429656794afaf220
jomoo02 <jomootgrn88@naver.com>
2024-07-04 23:53:58 +0900

    test: 첫 렌더링 시 모든 포켓몬 fetch

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,8 @@ import CardList from './ui/pokeCard/card-list';
 import { fetchPokes, fetchAllPoke } from './api/data';
 
 export default async function Page() {
-  const pokeData = await fetchPokes(0);
-  // const pokeData = await fetchAllPoke();
+  // const pokeData = await fetchPokes(0);
+  const pokeData = await fetchAllPoke();
   return (
     <div>
       <CardList initialPokeData={pokeData} />
--- app/ui/pokeCard/card-list.jsx
+++ app/ui/pokeCard/card-list.jsx
@@ -27,43 +27,43 @@ function CardListSkelton() {
 }
 
 export default function CardList({ initialPokeData }) {
-  const { isIntersecting, ref } = useIntersectionObserver();
+  // const { isIntersecting, ref } = useIntersectionObserver();
 
-  const {
-    pokeData: pokeDatas, fetchNextPage, hasNextPage, isFetchingNextPage, status, isLoading,
-  } = usePokeInfiniteQuery();
+  // const {
+  //   pokeData: pokeDatas, fetchNextPage, hasNextPage, isFetchingNextPage, status, isLoading,
+  // } = usePokeInfiniteQuery();
 
-  useEffect(() => {
-    console.log(hasNextPage, isIntersecting, isLoading, isFetchingNextPage);
-    if (hasNextPage && isIntersecting && !isFetchingNextPage) {
-      fetchNextPage();
-    }
-  }, [isIntersecting]);
+  // useEffect(() => {
+  //   console.log(hasNextPage, isIntersecting, isLoading, isFetchingNextPage);
+  //   if (hasNextPage && isIntersecting && !isFetchingNextPage) {
+  //     fetchNextPage();
+  //   }
+  // }, [isIntersecting]);
 
-  useEffect(() => {
-    const position = sessionStorage.getItem('pos');
-    console.log('1', position, status);
-    if (!isLoading && position) {
-      window.scrollTo({ top: position });
-    }
-  }, [isLoading]);
+  // useEffect(() => {
+  //   const position = sessionStorage.getItem('pos');
+  //   console.log('1', position, status);
+  //   if (!isLoading && position) {
+  //     window.scrollTo({ top: position });
+  //   }
+  // }, [isLoading]);
 
   return (
     <div className="flex w-full flex-col items-center min-h-screen">
       <div
         className="w-full grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-7 sm:gap-x-4 gap-y-3 sm:gap-y-4 justify-center items-center"
       >
-        {(isLoading) ? (
+        {initialPokeData.map((basicInfo, index) => (
+          <Card
+            key={basicInfo.id}
+            basicInfo={basicInfo}
+            priority={index <= 20}
+          />
+        ))}
+        {/* {(isLoading) ? (
           <CardListSkelton />
         ) : (
           <>
-            {initialPokeData.map((basicInfo, index) => (
-              <Card
-                key={basicInfo.id}
-                basicInfo={basicInfo}
-                priority={index <= 20}
-              />
-            ))}
             {pokeDatas.map((basicInfo) => (
               <Card
                 key={basicInfo.id}
@@ -71,9 +71,9 @@ export default function CardList({ initialPokeData }) {
               />
             ))}
           </>
-        )}
+        )} */}
       </div>
-      <div ref={ref} />
+      {/* <div ref={ref} /> */}
     </div>
   );
 }
