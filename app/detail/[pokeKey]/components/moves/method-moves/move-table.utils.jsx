897f8819abab20ebf2a3dab657b7448800efe989
jomoo02 <jomootgrn88@naver.com>
2024-08-28 22:10:08 +0900

    test: display-name(2)

--- app/detail/[pokeKey]/components/moves/method-moves/move-table.utils.jsx
+++ app/detail/[pokeKey]/components/moves/method-moves/move-table.utils.jsx
@@ -144,6 +144,7 @@ export function renderMoveCellWith(key, move, className) {
   };
 
   const renderFn = renderFnMap[key] || renderFnMap.name;
+  renderFn.displayName = `RenderMoveCellWith_${key}`;
 
   return renderFn(move, className);
 }
