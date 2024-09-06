3bdd4898ccbe1c676ad09309d618ec709384d273
jomoo02 <jomootgrn88@naver.com>
2024-09-06 16:17:14 +0900

    bug: useTrigger error 수정

--- app/detail/[pokeKey]/modules/evolution/hooks/useTrigger.js
+++ app/detail/[pokeKey]/modules/evolution/hooks/useTrigger.js
@@ -2,11 +2,19 @@ import { useLanguage } from '@/app/language-provider';
 import { getTriggerLocaleContent } from '../utils/trigger';
 
 export default function useTrigger(trigger, condition) {
+  const defaultTriggerContent = '';
+
   const { language } = useLanguage();
 
   const triggerContent = getTriggerLocaleContent(trigger, condition);
 
-  const localeContent = triggerContent[language] || triggerContent.ko || '';
+  if (!triggerContent) {
+    return {
+      triggerContent: defaultTriggerContent,
+    };
+  }
+
+  const localeContent = triggerContent[language] || triggerContent.ko || defaultTriggerContent;
 
   return {
     triggerContent: localeContent,
